a5c5d68252d27c2591ac4ded0cca7b30239cfe19
Miguel Arciniega <milearciniegara@ittepic.edu.mx>
2021-01-28 19:07:24 -0700

    Fixed Detalles

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -17,19 +17,17 @@ export class Tab1Page implements OnInit {
   constructor(private studentService: PlatillosService, private fb: FormBuilder) { }
 
   ngOnInit() {
-
-    this.myForm = this.fb.group(
-      {
-        name: [''],
-        image: [''],
-        description: [''],
-        price: [0],
-        ingredients: [''],
-      });
+    this.myForm = this.fb.group({
+      name: [''],
+      image: [''],
+      description: [''],
+      price: 0,
+      ingredients: ['']
+    });
+    this.cleanInputs();
   }
 
   create() {
-
     this.saurce = {
       name: this.myForm.controls.name.value,
       image: this.myForm.controls.image.value,
@@ -37,23 +35,18 @@ export class Tab1Page implements OnInit {
       price: this.myForm.controls.price.value,
       ingredients: this.myForm.controls.ingredients.value,
     };
-
     this.studentService.createPlatillo(this.saurce);
     this.cleanInputs();
-
+    alert('Platillo agregado con exito');
   }
 
   private cleanInputs(): void {
-    const regex = '(https?://)?([\\da-z.-]+)\\.([a-z.]{2,6})[/\\w .-]*/?';
     this.myForm = this.fb.group({
-      name: ['', Validators.compose([Validators.required, Validators.minLength(3), Validators.maxLength(150)])],
-      image: ['', Validators.compose([Validators.required, Validators.minLength(10), Validators.maxLength(300)])],
-      description: ['', Validators.compose([Validators.required, Validators.minLength(10), Validators.maxLength(300)])],
-      ingredients: ['', Validators.compose([Validators.required, Validators.minLength(10), Validators.maxLength(300)])],
-      price: [0, Validators.compose([Validators.required, Validators.pattern(/^-?(0|[1-9]\d*)?$/)])],
-      active: [true, Validators.compose([Validators.required])],
+      name: ['', Validators.compose([Validators.required, Validators.maxLength(30)])],
+      image: ['', Validators.compose([Validators.required, Validators.maxLength(300)])],
+      description: ['', Validators.compose([Validators.required, Validators.maxLength(300)])],
+      price: ['', Validators.compose([Validators.required])],
+      ingredients: ['', Validators.compose([Validators.required, Validators.maxLength(300)])]
     });
-
   }
-
 }
